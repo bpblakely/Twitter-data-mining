9b3c6cd21c53111eb5bfa82f2df1677a03f27c86
Brian Blakely <58829420+bpblakely@users.noreply.github.com>
2021-01-26 16:23:23 -0500

    Update text_cell.py

--- text_cell.py
+++ text_cell.py
@@ -22,6 +22,10 @@ def text_my_cell(text='program failed',trent=False):
                                 to='input_another_number',
                                 from_='input_twilio_number'
                             )
-                            
+                  
 # I use this code to send my self daily updates on the overall data collection every day
 # Useful to catch random bugs in Twitter's API that result in a request timeout and need to be corrected manually 
+
+# simple function to put commas in an integer to make it easily readable
+def int_comma(integer):
+    return ','.join([str(integer)[::-1][i:i+3] for i in range(0,len(str(integer)),3)])[::-1]
